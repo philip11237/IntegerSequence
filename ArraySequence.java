0864ca2a198be6a3dc1e1ece4d453c4b748b9e53
philip von mueffling <philip.vm3@gmail.com>
2020-12-08 21:36:17 -0500

    commit

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,5 +4,25 @@ public class ArraySequence implements IntegerSequence{
   int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+        currentIndex=0;
+         data = new int[other.length];
+         for (int i=0;i<other.length;i++){
+           data[i]=other[i];
+         }
+  }public ArraySequence(IntegerSequence otherseq){
+    currentIndex=0;
+     data = new int[otherseq.length()];
+     int i=0;
+     while (otherseq.hasNext){
+       data[i]=otherseq.next();
+       i++;
+     }
+     otherseq.reset();
+
 }
+}
+
+
+
+1.2.3.4.5
